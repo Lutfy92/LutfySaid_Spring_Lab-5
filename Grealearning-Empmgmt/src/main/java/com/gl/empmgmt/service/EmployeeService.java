455cb846d4f74a2f15e16f7f469fbe03a2f01884
Lutfy92 <115952827+Lutfy92@users.noreply.github.com>
2023-04-05 00:10:21 +0300

    Add files via upload

---
+++ Grealearning-Empmgmt/src/main/java/com/gl/empmgmt/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.gl.empmgmt.service;
+
+import java.util.List;
+
+import com.gl.empmgmt.model.Employee;
+
+public interface EmployeeService {
+	public List<Employee> getAllEmployees();
+
+	public	Employee saveEmployee(Employee employee);
+
+	public	Employee getEmployeeById(int id);
+
+	public	Employee updateEmployee(Employee employee);
+
+	public	void deleteEmployeeById(int  id);
+}
